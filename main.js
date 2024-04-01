768a50da258832a350143ca354924667b539008a
Moaydadauirshid <145578048+Moaydadauirshid@users.noreply.github.com>
2024-04-02 00:39:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img=document.querySelector('.img');
+let continer=document.querySelector('.continer');
+
+function phones(phone){
+    img.src=phone;
+}
+
+function colors (color){
+    continer.style.background=color;
+}
